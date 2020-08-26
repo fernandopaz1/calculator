a2bf6d1475aa2913726177a70de61c53aa9e18bc
Fernando Paz <ferpaz9292@gmail.com>
2020-08-25 22:27:13 -0300

    adding eventListener to operations

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -10,3 +10,36 @@ document.querySelectorAll("button.btnNumber").forEach(button=>{
     })    
 });
 
+document.querySelectorAll("button.btnOperation").forEach(button=>{
+    button.addEventListener("click",(e)=>{
+        let content=e.target.textContent;
+        if(content=="="){
+            display.innerHTML=resolve(input1,input2,operation);
+        }else if(content="C") clear();
+        else{
+            operation=content;
+        }
+    })    
+});
+
+function clear(){
+    var input1="";
+    var input2="";
+    var operation="";
+    display.innerHTML="";
+}
+
+function resolve(input1,input2,operation){
+    if(input1=="" || input2=="") return "Nan"
+    let int1=parseInt(input1);
+    let int2=parseInt(input2);
+    
+    switch(operation){
+        case "+": return int1+int2;
+        case "-": return int1-int2;
+        case "-": return int1*int2;
+        case "/":
+            return (int2==0) ? "Nan" : int1/int2;
+        default: return "Nan"
+    }
+}
