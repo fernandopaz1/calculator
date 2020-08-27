d7b5b0e6413361342af46fd0a2d8ad614b960504
Fernando Paz <ferpaz9292@gmail.com>
2020-08-26 21:09:35 -0300

    falta mejorar las funciones equalPress y operationPress

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,23 +4,29 @@ var input1="";
 var input2="";
 var operation="";
 
+function isAValidNumber(s){
+    if(s=="" || s=="Nan" || isNaN(s)) return false;
+    return true; 
+}
+
 document.querySelectorAll("button.btnNumber").forEach(button=>{
     button.addEventListener("click",(e)=>{
         display.innerHTML+=e.target.textContent;
+        input2= parseInt(display.textContent);
     })    
 });
 
 document.querySelectorAll("button.btnOperation").forEach(button=>{
     button.addEventListener("click",(e)=>{
         let content=e.target.textContent;
-        if(content=="="){
-            display.innerHTML=resolve(input1,input2,operation);
-        }else if(content="C") clear();
-        else{
-            operation=content;
-        }
+            operationPress(content);
+        
     })    
 });
+
+document.querySelector("button#btnEqual").addEventListener("click",resolve);
+
+document.querySelector("button#btnClear").addEventListener("click",clear);
 
 function clear(){
     var input1="";
@@ -29,7 +35,7 @@ function clear(){
     display.innerHTML="";
 }
 
-function resolve(input1,input2,operation){
+function resolve(){
     if(input1=="" || input2=="") return "Nan"
     let int1=parseInt(input1);
     let int2=parseInt(input2);
@@ -41,5 +47,24 @@ function resolve(input1,input2,operation){
         case "/":
             return (int2==0) ? "Nan" : int1/int2;
         default: return "Nan"
+    }
+}
+
+function equalPress(){
+    let inScreen=display.textContent;
+    if(input1=="" || isNaN(input1) && isAValidNumber(inScreen)) {
+        input1=parseInt(inScreen);
+    }else{input2=parseInt(display.textContent);
+        display.innerHTML=resolve(input1,input2,operation);}
+    
+}
+function operationPress(content){
+    if(input1!="" && input2!="" && operation!=""){
+        input1=resolve(input1,input2,operation);
+        display.innerHTML=input1;   
+    }
+    if(!isNaN(input2)){
+        input1=parseInt(display.textContent);
+        operation=content;
     }
 }
